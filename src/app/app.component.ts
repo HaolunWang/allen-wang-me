9bd4c600c6c69454260ce324f5ca509ca954a4f3
Allen Wang <allen.wang2017@yahoo.com>
2023-02-18 18:09:25 -0500

    try to make button disappear after click dropdown

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { TranslateService } from './i18n/translate.service';
 
 @Component({
@@ -6,17 +6,23 @@ import { TranslateService } from './i18n/translate.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
 
-  constructor(private translateService: TranslateService) {
+  constructor(private translateService: TranslateService, private elementRef: ElementRef) {
     console.log(translateService.data);
   }
 
-  setLang(lang: string) {
-    console.log(lang);
-    this.translateService.use(lang);
+  ngOnInit(): void {
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+    document.body.classList.toggle('dark-theme', prefersDark.matches);
   }
-  
+
+  // work with hideDropdownList()
+  ngAfterViewInit() {
+    this.dropdownClass = this.elementRef.nativeElement.querySelector('.dropdown-content');
+    console.log(this.dropdownClass.style.display);
+  }
+
   welcome = 'welcome';
   languages = 'languages';
   mySite = 'mySite';
@@ -31,9 +37,67 @@ export class AppComponent implements OnInit {
   footerDescription1 = 'footerDescription1';
   footerDescription2 = 'footerDescription2';
 
-  ngOnInit(): void {
-    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
-    document.body.classList.toggle('dark-theme', prefersDark.matches);
+  showDropdown:boolean = false;
+  dropdownClass: any;
+  // @ViewChild("sampleComponent") sampleComponent: ElementRef<any>;
+
+  setLang(lang: string) {
+    console.log(lang);
+    this.translateService.use(lang);
+  }
+
+  // show or hide dropdown list after clicking button - implement by normal way in Angular
+  showOrHideDropdownList() {
+    this.showDropdown = !this.showDropdown;
+    console.log("showDropdown: " + this.showDropdown);
+  }
+
+  showOrHideDropdownList1(lang: string) {
+    this.setLang(lang);
+    this.showDropdown = !this.showDropdown;
+    console.log("showDropdown: " + this.showDropdown);
+  }
+
+  //hide dropdown list after clicking dropdown option - implement by manipulate HTML element
+  hideDropdownList(lang: string) { // ? means parameter lang IS OPTIONAL
+    // this.dropdownClass.style.display = 'none';
+
+
+    // console.log("this.dropdownList.style.display: " + this.dropdownClass.style.display);
+    // if (this.dropdownClass.style.display === 'none') {
+    //   this.dropdownClass.style.display = 'block';
+    // } else if (this.dropdownClass.style.display === 'block') {
+    //   this.dropdownClass.style.display = 'none';
+    // }
+
+    console.log(lang);
+    this.setLang(lang);
+    var x = document.getElementById("dropdownlist");
+    if (x != null) {
+      console.log("before: " + x.style.display);
+      x.style.display = "none";
+      console.log("after: " + x.style.display);
+    }
+
+    // var x = document.getElementById("dropdownlist");
+    // if (x != null && x.style.display === "none") {
+    //   console.log(x.style.display);
+    //   x.style.display = "block"; 
+    // } else if (x != null && x.style.display === "block") {
+    //   console.log(x.style.display);
+    //   x.style.display = "none"; 
+    // }
+  }
+
+  showDropdownList() {
+    var x = document.getElementById("dropdownlist");
+    if (x != null) {
+      console.log("before: " + x.style.display);
+      x.style.display = "block";
+      console.log("after: " + x.style.display);
+    }
+
+    // this.dropdownClass.style.display = 'block';
   }
   
   toggleDarkTheme(): void {
